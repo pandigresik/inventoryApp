c46659fbc8299c78b368dde9e1e364ed6c3026bc
ahmad <ahmad.afandi85@gmail.com>
2020-12-30 05:24:35 +0700

    fix bug item terakhir recycleview tidak kelihatan, hapus scrollview secara default recycleview bisa scroll

--- app/src/main/java/com/asligresik/inventoryApp/fragment/HomeReportOutstandingStockRmiFragment.java
+++ app/src/main/java/com/asligresik/inventoryApp/fragment/HomeReportOutstandingStockRmiFragment.java
@@ -30,8 +30,6 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-;
-
 public class HomeReportOutstandingStockRmiFragment extends Fragment {
     View view;
     ProgressDialog loading;
